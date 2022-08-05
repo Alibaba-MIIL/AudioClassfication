66aee524ed9dc49af89de76ef58c500b829f1ac0
jqm <jinqing.mu@seeed.cc>
2022-08-05 14:33:03 +0800

    Fix data loading too slow and system path incompatibility

--- datasets/speechcommand_dataset.py
+++ datasets/speechcommand_dataset.py
@@ -1,11 +1,12 @@
 import torch
+import torchaudio
 import torch.nn.functional as F
-import librosa
 import os
 import glob
 import random
 from datasets.audio_augs import AudioAugs
 
+sep = os.path.sep
 
 class SpeechCommandsDataset(torch.utils.data.Dataset):
     def __init__(self,
@@ -22,19 +23,19 @@ class SpeechCommandsDataset(torch.utils.data.Dataset):
         if mode == 'train':
             fnames = self.get_training_list(root)
         elif mode == 'val':
-            fnames = self.load_meta_file(root, "/validation_list.txt")
+            fnames = self.load_meta_file(root, f"{sep}validation_list.txt")
         elif mode == 'test':
-            fnames = self.load_meta_file(root, "/testing_list.txt")
+            fnames = self.load_meta_file(root, f"{sep}testing_list.txt")
         else:
             raise ValueError
         self.audio_files = sorted(fnames)
         self.label2idx = dict(zip(self.labels, range(len(self.labels))))
-        self.transforms = AudioAugs(transforms, sampling_rate, p=0.5) if transforms is not None else None
+        self.transforms = transforms
         self.use_background = use_background
         if self.use_background:
-            self.bg_aug = glob.glob(root + "/_background_noise_/*.wav")
+            self.bg_aug = glob.glob(root + f"{sep}_background_noise_{sep}*.wav")
             self.bg_aug = [f for f in self.bg_aug if 'noise' not in os.path.basename(f)]
-            self.bg_aug = [torch.from_numpy(librosa.core.load(f, sr=sampling_rate, mono=True)[0]) for f in self.bg_aug]
+            self.bg_aug = [torchaudio.load(f)[0][0].detach() for f in self.bg_aug]
             self.bg_aug = [x for x in self.bg_aug]
 
     def load_meta_file(self, root, f_meta):
@@ -44,31 +45,34 @@ class SpeechCommandsDataset(torch.utils.data.Dataset):
         return fnames
 
     def _get_labels(self, root):
-        f_names = glob.glob(root + "/**/*.wav")
-        self.labels = sorted(list(set([f.split('/')[-2] for f in f_names])))
+        f_names = glob.glob(root + f"{sep}**{sep}*.wav")
+        self.labels = sorted(list(set([f.split(f'{os.path.sep}')[-2] for f in f_names])))
         self.labels = sorted([l for l in self.labels if l != '_background_noise_'])
 
     def __getitem__(self, index):
         fname = self.audio_files[index]
-        label = fname.split('/')[-2]
+        if '/' in fname:
+            fname = fname.replace('/',sep)
+        label = fname.split(f'{sep}')[-2]
         label = self.label2idx[label]
-        audio, sampling_rate = librosa.core.load(fname, sr=None, mono=True)
-        audio = 0.95*librosa.util.normalize(audio)
-        audio = torch.from_numpy(audio).float()
+        audio, sampling_rate = torchaudio.load(fname)
+        audio.squeeze_()
+        audio = 0.95 * (audio / audio.__abs__().max()).float()
 
-        assert("sampling rate of the file is not as configured in dataset, will cause slow fetch {}".format(sampling_rate != self.sampling_rate))
+        assert ("sampling rate of the file is not as configured in dataset, will cause slow fetch {}".format(
+            sampling_rate != self.sampling_rate))
         if audio.shape[0] >= self.segment_length:
             max_audio_start = audio.size(0) - self.segment_length
             audio_start = random.randint(0, max_audio_start)
-            audio = audio[audio_start : audio_start + self.segment_length]
+            audio = audio[audio_start: audio_start + self.segment_length]
         else:
             audio = F.pad(
                 audio, (0, self.segment_length - audio.size(0)), "constant"
             ).data
 
         if self.use_background and random.random() < 0.5:
-            i = random.randint(0, len(self.bg_aug)-1)
-            s_level = (audio**2).mean()
+            i = random.randint(0, len(self.bg_aug) - 1)
+            s_level = (audio ** 2).mean()
             bg = self.bg_aug[i]
             if bg.shape[0] >= self.segment_length:
                 max_bg_start = bg.size(0) - self.segment_length
@@ -80,24 +84,24 @@ class SpeechCommandsDataset(torch.utils.data.Dataset):
                 ).data
             bg_level = (bg ** 2).mean().sqrt()
             snr_db = 20 + random.random() * 5
-            sgm = s_level*10**(-snr_db/10)
+            sgm = s_level * 10 ** (-snr_db / 10)
             bg /= bg_level
-            audio.add_(bg*sgm)
+            audio.add_(bg * sgm)
 
         if self.transforms is not None:
-            audio = self.transforms(audio)
+            audio = AudioAugs(self.transforms, sampling_rate, p=0.5)(audio)
         return audio.unsqueeze(0), label
 
     def __len__(self):
         return len(self.audio_files)
 
     def get_training_list(self, root):
-        f_names = glob.glob(root + "/**/*.wav")
+        f_names = glob.glob(root + f"{sep}**{sep}*.wav")
         f_names = [f for f in f_names if os.path.basename(os.path.dirname(f)) != '_background_noise_']
-        val = self.load_meta_file(root, "/validation_list.txt")
-        test = self.load_meta_file(root, "/testing_list.txt")
+        val = self.load_meta_file(root, f"{sep}validation_list.txt")
+        test = self.load_meta_file(root, f"{sep}testing_list.txt")
         valtest = val + test
-        train = list(set(f_names)-set(valtest))
+        train = list(set(f_names) - set(valtest))
         return train
 
 
